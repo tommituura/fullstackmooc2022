604c26861c5eaf9129c3fb1cd1e3da28d68dd2bd
Tommi Tuura <tommi.tuura@helsinki.fi>
2022-12-03 14:59:17 +0200

    1.6 unicafe step1

--- osa1/unicafe/src/App.js
+++ osa1/unicafe/src/App.js
@@ -7,9 +7,23 @@ const App = () => {
   const [bad, setBad] = useState(0)
 
   return (
+    <>
     <div>
-      code here
+      <h1>give feedback</h1>
+      <p>
+        <button onClick={() => setGood(good + 1)}>good</button>
+        <button onClick={() => setNeutral(neutral + 1)}>neutral</button>
+        <button onClick={() => setBad(bad + 1)}>bad</button>
+      </p>
     </div>
+    <div>
+      <h1>statistics</h1>
+      <p>good {good}</p>
+      <p>neutral {neutral}</p>
+      <p>bad {bad}</p>
+    </div>
+    </>
+
   )
 }
 
