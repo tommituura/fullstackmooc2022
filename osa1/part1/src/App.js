8709f29d71bd4251239eddeba235d488380238c6
Tommi Tuura <tommi.tuura@helsinki.fi>
2022-12-03 14:16:45 +0200

    komponenttien refaktorointi

--- osa1/part1/src/App.js
+++ osa1/part1/src/App.js
@@ -1,18 +1,13 @@
 import { useState } from 'react'
 
-const Display = (props) => {
-  return (
-    <div>{props.counter}</div>
-  )
-}
+const Display = ({ counter }) => <div>{counter}</div>
 
-const Button = (props) => {
-  return (
-    <button onClick={props.handleClick}>
-      {props.text}
+const Button = ({ handleClick, text }) => (
+  <button onClick={handleClick}>
+    {text}
   </button>
-  )
-}
+)
+
 
 const App = () => {
   const [ counter, setCounter ] = useState(0)
