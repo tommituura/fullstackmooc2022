eb3dd109e0d0dcc48df3b1b3b360bf7882578ddb
Tommi Tuura <tommi.tuura@helsinki.fi>
2022-12-04 00:07:29 +0200

    1.9 unicafe step4

--- osa1/unicafe/src/App.js
+++ osa1/unicafe/src/App.js
@@ -1,6 +1,14 @@
 import { useState } from 'react'
 
 const Statistics = ({good, neutral, bad, all, avg, positivePercentage}) => {
+  if (all === 0) {
+    return (
+      <div>
+        <h1>statistics</h1>
+        <p>No feedback given</p>
+      </div>
+    )
+  }
   return (
     <div>
       <h1>statistics</h1>
