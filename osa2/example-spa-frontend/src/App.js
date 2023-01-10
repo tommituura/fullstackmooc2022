b2c67a6743471a9ede3372d6069a38de641cd813
Tommi Tuura <tommi.tuura@helsinki.fi>
2023-01-11 01:35:40 +0200

    virheenkäsittelyä ym.. (part 2-6 tila)

--- osa2/example-spa-frontend/src/App.js
+++ osa2/example-spa-frontend/src/App.js
@@ -24,6 +24,12 @@ const App = () => {
       .then(returnedNote => {
         setNotes(notes.map(note => note.id === id ? returnedNote : note ))
       })
+      .catch(error => {
+        alert(
+          `the note '${note.content}' was already deleted from server`
+        )
+        setNotes(notes.filter(n => n.id !== id))
+      })
   }
 
   const addNote = (event) => {
